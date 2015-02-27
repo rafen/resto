58acd4345ad08789e40d5bf2844b22c23e47c1bd
rafen <capdevielle@gmail.com>
2015-02-27 02:41:27 -0300

    Added admin interface for comments

--- restaurants/admin.py
+++ restaurants/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
-from .models import Restaurant, Visit
+from .models import Restaurant, Visit, Comment
 
 
 class VisitInline(admin.StackedInline):
     model = Visit
+    extra = 1
+
+
+class CommentInline(admin.StackedInline):
+    model = Comment
     extra = 1
 
 
@@ -12,7 +17,7 @@ class RestaurantAdmin(admin.ModelAdmin):
     list_display_links = ('id', 'name')
     search_fields = ('name', 'description')
     list_filter = ('active', 'created')
-    inlines = (VisitInline,)
+    inlines = (VisitInline, CommentInline)
 
 
 admin.site.register(Restaurant, RestaurantAdmin)
